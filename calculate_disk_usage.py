f7420a6584216830c3187ea92125cc7bcb206fa1
Ibrahim Musayev <84103775+Codehunter-py@users.noreply.github.com>
2022-04-27 11:08:56 +0200

    added desired capacity function

--- calculate_disk_usage.py
+++ calculate_disk_usage.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 
+################################################################################
+# Name    : calculate_disk_usage.py                                            #
+# Author  : Ibrahim Musayev                                                    #
+# Purpose : Script does compute disk usage of /opt/bsi and elasticsearch's     #
+#           snapshots and gives an overview analysis of filesystem             #
+# History : 14-01-22 : creation                                                #
+################################################################################
+
 import re
 import subprocess
 
@@ -12,6 +20,19 @@ def diskCapacity():
     result = re.findall(pattern, opt_bsi_size)
     result = float(result[0]) if '.' in result[0] else int(result[0])
     return result
+
+def desiredCapacity():
+    return (70 * diskCapacity()) / 100
+
+def diskUsed():
+    """Functions returns integer/float that represents used size of /opt/bsi
+    in human readable format with gigabytes """
+    
+    opt_bsi_used = subprocess.check_output("df -h /opt/bsi", shell=True).decode()
+    pattern_used = r"(\d+(?:\.\d+)?)"
+    result_used = re.findall(pattern_used, opt_bsi_used)
+    result_used = float(result_used[1]) if '.' in result_used[1] else int(result_used[1])
+    return result_used
 
 def elkSnapsSize():
     """Function returns integer/float that represents how much gigabytes elasticsearch's 
@@ -36,24 +57,24 @@ def diskAvail():
 def calculate():
     """ Function displays how much elasticsearch's montly snaps could fit in available space"""
     print("\nTotal size of /opt/bsi is:", diskCapacity(),"G\n")
+    print("Desired capacity or 70% of /opt/bsi is:", desiredCapacity(),"G\n")
     print("Available space:", diskAvail(),"G\n")
     print("Total size of elasticsearch's snaps:", elkSnapsSize(),"G\n")
     snaps_count = int(input("Enter total number of monthly snaps: "))
     print(f"\nSince montly-snaps policy was defined, {snaps_count} snapshot(s) captured which one(s) take {elkSnapsSize()}G space all together.\n")
 
-    total_snaps_size = elkSnapsSize()
     if snaps_count > 1:
         try:
-            each_snap = total_snaps_size / snaps_count
+            each_snap = elkSnapsSize() / snaps_count
             print(f"Each snapshot needs around ~{each_snap:.2f}G for every month.")
         except ZeroDivisionError:
             pass
     
-    display = input("\nDisplay number of monthly snaps could fit in available space: y/n ")
+    display = input("\nDisplay number of monthly snaps could fit in the /opt/bsi available space until reach out the desired capacity: y/n ")
 
     if display == "y":
-        diskAvailable = diskAvail()
-        result = diskAvailable / each_snap
-        print(f"\nApproxmately {result:.1f} monthly snap(s) could fit in the /opt/bsi available space.")
+        result = desiredCapacity() - diskUsed()
+        result = result / each_snap
+        print(f"\nApproxmately {result:.1f} monthly snap(s) could fit in the /opt/bsi available space until reach out the desired capacity.")
 
 calculate()
